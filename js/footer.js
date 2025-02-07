7c8078f1473f9dc7ea92534810eeddc6d6d5c874
Mehdi <mehdiennajehfr@gmail.com>
2025-02-07 15:44:58 +0100

    fini

--- js/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("/pages/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-container").innerHTML = data;
-        })
-        .catch(error => console.error("Erreur lors du chargement du footer :", error));
-});
--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("/pages/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container").innerHTML = data;
-        })
-        .catch(error => console.error("Erreur lors du chargement de la navbar :", error));
-});
