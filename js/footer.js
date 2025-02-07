3860545c4f456d3e3306437a4c52aeb938766056
Mehdi <mehdiennajehfr@gmail.com>
2025-02-07 15:30:24 +0100

    fini

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/pages/footer.html")
+    fetch("footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
